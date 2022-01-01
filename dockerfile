fcd3800966367f9bddc6fa46f43c509c1fa79cc1
Andrew Hargrave <me@andyh.app>
2022-01-01 16:53:36 +0000

    add action

--- dockerfile
+++ dockerfile
@@ -1,13 +1,6 @@
-FROM bitnami/git as git
-WORKDIR /app
-RUN git clone --recursive https://github.com/BlueMap-Minecraft/BlueMap.git bluemap
-
 FROM openjdk:17 as java
+RUN mkdir /app
 WORKDIR /app
-COPY --from=git /app/bluemap /app
-COPY ./BlueMap-1.7.2-cli.jar /app/bluemap/build/release/bluemap.jar
-# RUN npx browserslist@latest --update-db
-# RUN ./gradlew clean build
 COPY ./start.sh /app
 COPY ./core.conf /app
 
--- start.sh
+++ start.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+echo "Downloading bluemap latest"
+
 
 if [ -f "/bluemap/bluemap.jar" ]
     echo "File already exists, skipping step..."
 then
     mkdir -p /bluemap
-    cp /app/bluemap/build/release/bluemap.jar /bluemap/bluemap.jar
+
+    LOCATION=$(curl -s https://api.github.com/repos/BlueMap-Minecraft/BlueMap/releases/latest \
+    | grep "tag_name" \
+    | awk '{print "https://github.com/BlueMap-Minecraft/BlueMap/releases/download/" substr($2, 2, length($2)-3) "/BlueMap-" substr($2, 3, length($2)-4) "-cli.jar"}') \
+    ; curl -L -o bluemap.jar $LOCATION
+
+    cp /app/bluemap.jar /bluemap/bluemap.jar
     cp /app/core.conf /bluemap/core.conf
 fi
 
